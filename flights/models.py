b0a738ca28de18ace2726427b3d03cd36b21894b
Abdul-Salam15 <abdulsalamadebayo0410@gmail.com>
2025-07-31 17:55:04 +0100

    Link the Flight model to the Airport model using FK

--- flights/models.py
+++ flights/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Airport(models.Model):
     code = models.CharField(max_length = 3)
     city = models.CharField(max_length = 64)
-    
+
 # return f"{self. city} ({self.code})" this gives for example, Lagos (LAG)
     def __str__(self):
         return f"{self. city} ({self.code})"
@@ -12,8 +12,8 @@ class Airport(models.Model):
 
 
 class Flight(models.Model):
-    origin = models.CharField(max_length = 64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete = models.CASCADE, related_name = "departures")
+    destination = models.ForeignKey(Airport, on_delete = models.CASCADE, related_name = "arrivals")
     duration = models.IntegerField()
 
     def __str__(self):
